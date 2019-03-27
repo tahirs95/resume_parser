cb510da6e3efec8df2b10b3c49c86a7aaa5cbdff
tahirs95 <tahirs95@hotmail.com>
2019-03-27 21:23:16 +0500

    Extracting Skills.

--- resumeparser.py
+++ resumeparser.py
@@ -24,6 +24,23 @@ worksheet = workbook.add_worksheet()
 files_list = glob.glob('./Files/*.pdf')
 print(files_list)
 
+new_list = []
+
+with open('skills.csv', 'rb') as f:
+    reader = csv.reader(f)
+    your_list = list(reader)
+
+# with open('nontechnicalskills.csv', 'rb') as f:
+#     reader = csv.reader(f)
+#     your_list1 = list(reader)
+
+s = set(your_list[0])
+s1 = your_list
+# s2 = your_listatt
+skillindex = []
+skills = []
+skillsatt = []
+
 #Function converting pdf to string
 def convert(fname, pages=None):
     if not pages:
@@ -42,7 +59,7 @@ def convert(fname, pages=None):
     text = output.getvalue()
     output.close
     return text
-    
+
 #Function to extract names from the string using spacy
 def extract_name(string):
     r1 = unicode(string)
@@ -50,8 +67,8 @@ def extract_name(string):
     doc = nlp(r1)
     for ent in doc.ents:
         if(ent.label_ == 'PER'):
+            global new_list
             first = str(ent.text)
-            print(first)
             break
     return first
 
@@ -60,6 +77,7 @@ def extract_phone_numbers(string):
     r = re.compile(r'(\d{3}[-\.\s]??\d{3}[-\.\s]??\d{6}|\d{3}[-\.\s]??\d{3}[-\.\s]??\d{5}|\d{3}[-\.\s]??\d{3}[-\.\s]??\d{4})')
     phone_numbers = r.findall(string)
     return [re.sub(r'\D', '', number) for number in phone_numbers]
+
 #Function to extract Email address from a string using regular expressions
 def extract_email_addresses(string):
     r = re.compile(r'[\w\.-]+@[\w\.-]+')
@@ -68,39 +86,51 @@ def extract_email_addresses(string):
 worksheet.write(0, 0, "Name")
 worksheet.write(0, 1, "Phone Number")
 worksheet.write(0, 2, "Email")
-
+worksheet.write(0, 3, "Skills")
 try:
-    for var, i in enumerate(files_list):
-        resume_string = convert(i)
-        resume_string1 = resume_string
-        #Removing commas in the resume for an effecient check
-        resume_string = resume_string.replace(',',' ')
-        #Converting all the charachters in lower case
-        resume_string = resume_string.lower()
-        first = extract_name(resume_string1)
-        print('\n')
-        print('Phone Number is')
-        y = extract_phone_numbers(resume_string)
-        y1 = []
-        for i in range(len(y)):
-            if(len(y[i])>9):
-                y1.append(y[i])
-        print(y1)
-        print('\n')
-        print('Email id is')
-        print(extract_email_addresses(resume_string))
-        third = extract_email_addresses(resume_string)
-        l1 = var+1
-        if first[0:25]:
-            worksheet.write(l1, 0, first[0:25])
-        if y1:
-            worksheet.write(l1, 1, y1[0])
-        if third:
-            worksheet.write(l1, 2, third[0])
-    workbook.close()
+	for var, i in enumerate(files_list):
+	    try:
+		resume_string = convert(i)
+		resume_string1 = resume_string
+		#Removing commas in the resume for an effecient check
+		resume_string = resume_string.replace(',',' ')
+		#Converting all the charachters in lower case
+		resume_string = resume_string.lower()
+		first = extract_name(resume_string1)
+		y = extract_phone_numbers(resume_string)
+		y1 = []
+		for i in range(len(y)):
+		    if(len(y[i])>9):
+			y1.append(y[i])
+		new_list.append(y1)
+		third = extract_email_addresses(resume_string)
+		new_list.append(third)
+		print(first, y1, third)
 
+		for word in resume_string.split(" "):
+			if word in s:
+				skills.append(word)
+		skills1 = list(set(skills))
+		print("Following are his/her Technical Skills")
+		print(skills1)
+		print('\n')		
+
+		l1 = var+1
+		if first[0:25]:
+		    worksheet.write(l1, 0, first[0:25])
+		if y1:
+		    worksheet.write(l1, 1, y1[0])
+		if third:
+		    worksheet.write(l1, 2, third[0])
+		if skills1:
+			worksheet.write(l1, 3, str(skills1))
+	    except Exception as ex:
+			print(str(ex))
+			pass	
 except Exception as ex:
-        print (str(ex))
-        workbook.close()
+	print(str(ex))
+	workbook.close()
+
+workbook.close()
 
 
